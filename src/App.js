8c6ff894d2496fcf4a23e9428bf461c99247754a
Garyun Young <garyunyoung@gmail.com>
2019-03-04 20:40:36 +0000

    Add basic layout

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="task-container">
+        <input
+          className="task-input"
+          type="text"
+          name="name"
+          placeholder="Your task here"
+        />
+        <section className="task-table">
+          <ul className="task-list">
+            <li className="task-list__item">Design React.js app</li>
+            <li className="task-list__item">Make React.js app</li>
+            <li className="task-list__item">Finish React.js app</li>
+          </ul>
+        </section>
       </div>
     );
   }
 }
+
 
 export default App;
