6cba655e4c5eea38ad6cfea2e00fe3a1d2df2255
Garyun Young <garyunyoung@gmail.com>
2019-03-09 14:29:59 +0000

    Add basic functionality

--- src/App.js
+++ src/App.js
@@ -1,27 +1,53 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [],
+      currentValue: []
+    };
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const newTask = this.newTask.value;
+
+    this.setState({
+      list: [...this.state.list, newTask]
+    });
+  };
+
+  handleInputData = event => {
+    this.setState({ ...this.state, currentValue: event.target.value });
+  };
+
   render() {
     return (
       <div className="task-container">
-        <input
-          className="task-input"
-          type="text"
-          name="name"
-          placeholder="Your task here"
-        />
+        <form className="task-form" onSubmit={this.handleSubmit}>
+          <input
+            className="task-input"
+            type="text"
+            name="newTask"
+            placeholder="Your task here"
+            ref={input => (this.newTask = input)}
+            onChange={this.handleInputData}
+          />
+          {/* <input className="submit-button" type="submit" value="Add task!"
+          /> */}
+        </form>
         <section className="task-table">
           <ul className="task-list">
-            <li className="task-list__item">Design React.js app</li>
-            <li className="task-list__item">Make React.js app</li>
-            <li className="task-list__item">Finish React.js app</li>
+            {this.state.list.map(listItem => {
+              return <li className="task-list__item">{listItem}</li>;
+            })}
           </ul>
         </section>
       </div>
     );
   }
 }
-
 
 export default App;
