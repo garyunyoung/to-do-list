23d6f4452a421fcf1dd2e668697063108f61a413
Garyun Young <garyunyoung@gmail.com>
2019-03-10 14:05:06 +0000

    Refactor code

--- src/Tasklist.js
+++ src/Tasklist.js
@@ -14,20 +14,28 @@ export default function TaskList(props) {
             onDelete={() => props.onDelete(i)}
           />
         ))}
-        <button
-          className={`task-list__clear-button ${
-            props.list.length === 0 ? "hidden" : ""
-          }`}
-          onClick={props.onClear}
-        >
-          clear items!
-        </button>
-        {props.list.length === 0 ? (
-          <p className="task-list__comment">there are no tasks in your list!</p>
-        ) : null}
+        <ClearButton list={props.list} onClear={props.onClear} />
+        <EmptyListComment visible={props.list.length === 0} />
       </ul>
     </section>
   );
+}
+
+function ClearButton(props) {
+  const buttonState = props.list.length === 0 ? "hidden" : "";
+  return (
+    <button
+      className={`task-list__clear-button ${buttonState}`}
+      onClick={props.onClear}
+    >
+      clear items!
+    </button>
+  );
+}
+
+function EmptyListComment(props) {
+  if (!props.visible) return null;
+  return <p className="task-list__comment">there are no tasks in your list!</p>;
 }
 
 TaskList.propTypes = {
